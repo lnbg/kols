8d8f0947736cc13ab42909e2acc1779717e82142
Le Nam <ltnam2804@gmail.com>
2018-04-10 14:54:31 +0700

    fix criteria get facebook analytics data

--- app/Criteria/Facebook/GetFacebookAnalyticsCriteria.php
+++ app/Criteria/Facebook/GetFacebookAnalyticsCriteria.php
@@ -22,11 +22,10 @@ class GetFacebookAnalyticsCriteria implements CriteriaInterface
      */
     public function apply($model, RepositoryInterface $repository)
     {
-        $model = $model->leftJoin('facebook_posts', 'facebook_profiles.id', 'facebook_posts.profile_id')
-        ->select(\DB::raw('facebook_profiles.id, facebook_profiles.facebook_id, facebook_profiles.type,
+        $model = $model->select(\DB::raw('facebook_profiles.id, facebook_profiles.facebook_id, facebook_profiles.type,
         facebook_profiles.name, facebook_profiles.username, facebook_profiles.fan_count, facebook_profiles.picture,
-        facebook_profiles.verification_status,post_count, share_count, interaction_count, reaction_count, comment_count'))
-        //sum(like_count + haha_count + wow_count + love_count + sad_count + thankful_count + share_count + comment_count) as interaction_count'))
+        facebook_profiles.verification_status, post_count, facebook_profiles.share_count, facebook_profiles.interaction_count, 
+        facebook_profiles.reaction_count, facebook_profiles.comment_count'))
         ->groupBy('facebook_profiles.id')
         ->orderBy('facebook_profiles.fan_count', 'desc');
         return $model;
